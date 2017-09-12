5684f2696ff7206ddb24b5a1fb7269486485dfd1
bryanh210 <tuongh@sas.upenn.edu>
2017-09-12 15:27:36 -0700

    Putting books to shelves

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -30,6 +30,9 @@ class BookShelf extends Component{
                         </div>
                         )
                     }
+                    else{
+                      console.log('Nope')
+                    }
               })}
                   </div>
             </li>
